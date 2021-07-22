c9837067c95828d4069772dc7600efc5c4a86118
SKIV-2611 <didm.platonov@yandex.ru>
2021-07-22 14:59:35 +0300

    Добавьте файлы проекта.

---
+++ Migrations/20210722115325_Initial-Create.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AltPaymentApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payment",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DboID = table.Column<int>(type: "int", nullable: false),
+                    PayerAccountNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReceiverAccountNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PaymentDetails = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payment", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payment");
+        }
+    }
+}
---
+++ Models/Statement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AltPaymentApi.Models
+{
+    public class Statement
+    {
+        public string DebitAccountNumber { get; set; }
+        public string CreditAccountNumber { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
