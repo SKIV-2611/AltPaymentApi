809e9ca4f4fa1a77eea4518062720644c0a4fa22
SKIV-2611 <didm.platonov@yandex.ru>
2021-07-28 17:04:31 +0300

    Deleting GET, PUT and DELETE methods and common refactoring

--- Controllers/PaymentsController.cs
+++ Controllers/PaymentsController.cs
@@ -20,60 +20,6 @@ namespace AltPaymentApi.Controllers
             _context = context;
         }
 
-        // GET: api/Payments
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<PaymentDTO>>> GetPayments()
-        {
-            return await _context.Payments
-                .Select(x => ItemToDTO(x))
-                .ToListAsync();
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<PaymentDTO>> GetPayment(long id)
-        {
-            var Payment = await _context.Payments.FindAsync(id);
-
-            if (Payment == null)
-            {
-                return NotFound();
-            }
-
-            return ItemToDTO(Payment);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdatePayment(long id, PaymentDTO PaymentDTO)
-        {
-            if (id != PaymentDTO.ID)
-            {
-                return BadRequest();
-            }
-
-            var Payment = await _context.Payments.FindAsync(id);
-            if (Payment == null)
-            {
-                return NotFound();
-            }
-
-            Payment.DboID = PaymentDTO.DboID;
-            Payment.PayerAccountNumber = PaymentDTO.PayerAccountNumber;
-            Payment.ReceiverAccountNumber = PaymentDTO.ReceiverAccountNumber;
-            Payment.Amount = PaymentDTO.Amount;
-            Payment.PaymentDetails = PaymentDTO.PaymentDetails;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException) when (!PaymentExists(id))
-            {
-                return NotFound();
-            }
-
-            return NoContent();
-        }
-
         [HttpPost]
         public async Task<ActionResult<PaymentDTO>> CreatePayment(PaymentDTO PaymentDTO)
         {
@@ -97,41 +43,10 @@ namespace AltPaymentApi.Controllers
             return Ok();
         }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePayment(long id)
-        {
-            var Payment = await _context.Payments.FindAsync(id);
-
-            if (Payment == null)
-            {
-                return NotFound();
-            }
-
-            _context.Payments.Remove(Payment);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
         private bool PaymentExists(long id) =>
              _context.Payments.Any(e => e.ID == id);
 
         private bool PaymentExistsByDboID(long DboID) =>
             _context.Payments.Any(e => e.DboID == DboID);
-
-        private static PaymentDTO ItemToDTO(Payment Payment) =>
-            new PaymentDTO
-            {
-                ID = Payment.ID,
-                DboID = Payment.DboID,
-
-                PayerAccountNumber = Payment.PayerAccountNumber,
-
-                ReceiverAccountNumber = Payment.ReceiverAccountNumber,
-
-                Amount = Payment.Amount,
-
-                PaymentDetails = Payment.PaymentDetails
-        };
     }
 }
