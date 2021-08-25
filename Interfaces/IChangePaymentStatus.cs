3702d7963a3028bef7e8e18d9dbc97ce88bd2aaa
SKIV-2611 <didm.platonov@yandex.ru>
2021-08-25 09:06:37 +0300

    Started adding RestEase implementation

---
+++ Interfaces/IChangePaymentStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestEase;
+using AltPaymentApi.Models;
+
+namespace AltPaymentApi.Interfaces
+{
+    public interface IChangePaymentStatus
+    {
+        [Put("api/PlugPaymentOrders/id?{status}")]
+        public Task ChangePaymentStatus
+            ([Query("id")] int id, [Query("status")] string status,
+            [Body] PaymentDTO paymentDTO);
+    }
+}
